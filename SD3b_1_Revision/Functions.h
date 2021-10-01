4bf6eb021498540d6040f5097a6a89a2cbe0a393
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-10-01 10:48:23 +0100

    Added code to test swap_ptr

--- SD3b_1_Revision/Functions.h
+++ SD3b_1_Revision/Functions.h
@@ -1,16 +1,15 @@
 #pragma once
 //function, if inside a class we would call it a method definition
 
-
-//EX - 1 
+//EX - 1
 /// @brief Adds two integer values
 /// @param x Integer
 /// @param y Integer
 /// @return Integer sum
 int add(int x, int y) {
-    //TODO - Add exception handling
-    //HACK - Solves problem but...
-    return x + y;
+	//TODO - Add exception handling
+	//HACK - Solves problem but...
+	return x + y;
 }
 
 //EX - 2
@@ -18,24 +17,23 @@ int add(int x, int y) {
 /// @param x Integer
 /// @param y Integer
 void swap(int x, int y) {
-
-    int temp = y;
-    y = x;
-    x = temp;
+	int temp = y;
+	y = x;
+	x = temp;
 }
 
 //EX - 3
 void swap_ref(int& x, int& y)
 {
-    int temp = y;
-    y = x;
-    x = temp;
+	int temp = y;
+	y = x;
+	x = temp;
 }
 
 //EX - 4
 void swap_ptr(int* px, int* py)
 {
-    int* ptemp = py;
-    py = px;
-    px = ptemp;
+	int* ptemp = py;
+	py = px;
+	px = ptemp;
 }
--- SD3b_1_Revision/SD3b_1_Revision.cpp
+++ SD3b_1_Revision/SD3b_1_Revision.cpp
@@ -7,16 +7,35 @@
 
 int main()
 {
-    //test add
-    std::cout << "Add: " << add(3, 2) << std::endl;
+	//test add
+	std::cout << "Add: " << add(3, 2) << std::endl;
 
-    //test swap vers. 1
-    int a = 10, b = 50;
-    swap(a, b);
-    std::cout << a << ", " << b << std::endl;
-    std::cout << "Remember an int is a value-type so we pass-by-copy" << std::endl;
-    std::cout << "What are the other value-types?" << std::endl;
+	//test swap vers. 1
+	int a = 10, b = 50;
+	swap(a, b);
+	std::cout << a << ", " << b << std::endl;
+	std::cout << "Remember an int is a value-type so we pass-by-copy" << std::endl;
+	std::cout << "What are the other value-types?" << std::endl;
 
+	std::cout << std::endl;
 
+	//test swap vers. 2
+	int c = 10, d = 50;
+	swap_ref(c, d);
+	std::cout << c << ", " << d << std::endl;
+	std::cout << "Remember when we use & we pass an address of the variable, just like pass-by-reference" << std::endl;
+
+	//test swap vers. 3
+	int e = 200;
+	int f = 500;
+	int* pE = &e;
+	int* pF = &f;
+	swap_ptr(pE, pF);
+	std::cout << *pE << ", " << *pF << std::endl;
+	std::cout << "Remember when we use & we pass an address of the variable, just like pass-by-reference" << std::endl;
+
+	//test initializing pointer with address of an int and showing address
+	int test = 34;
+	int* pTest = &test;
+	std::cout << "Address of pTest: " << pTest << std::endl;
 }
-
