09235ddd84e5bbbf67c1a143b87cb27bcf9469f9
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-10-01 10:33:52 +0100

    Added comments to swap test

--- SD3b_1_Revision/SD3b_1_Revision.cpp
+++ SD3b_1_Revision/SD3b_1_Revision.cpp
@@ -7,6 +7,16 @@
 
 int main()
 {
-    std::cout << "Add: " << add(3, 2);
+    //test add
+    std::cout << "Add: " << add(3, 2) << std::endl;
+
+    //test swap vers. 1
+    int a = 10, b = 50;
+    swap(a, b);
+    std::cout << a << ", " << b << std::endl;
+    std::cout << "Remember an int is a value-type so we pass-by-copy" << std::endl;
+    std::cout << "What are the other value-types?" << std::endl;
+
+
 }
 
