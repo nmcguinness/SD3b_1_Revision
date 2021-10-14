b2533b3ffc75c7d65b945b2aa660fa149aebc97e
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-10-14 09:38:05 +0100

    Added constructors and getters

--- SD3b_1_Revision/Flower.h
+++ SD3b_1_Revision/Flower.h
@@ -20,3 +20,27 @@ public:
 	double getPrice() const;
 	bool operator==(Flower other);
 };
+
+string Flower::getName() const {
+	return this->name;
+}
+double Flower::getPrice() const {
+	return this->price;
+}
+
+//Flower::Flower() : name(""), petals(0), price(0) {}
+Flower::Flower() {
+	//(*this).name = "";
+	this->name = "";
+	this->petals = 0;
+	this->price = 0;
+}
+
+//Flower::Flower(string name, int petals, double price) :
+//	name(name), petals(petals), price(price) {}
+
+Flower::Flower(string name, int petals, double price) {
+	this->name = name;
+	this->petals = petals;
+	this->price = price;
+}
